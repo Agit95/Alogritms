bd775f14a5cb2f8d190152065be14e9a5bd14757
Your Name <you@example.com>
2018-02-15 20:36:55 +0400

    Add String reverse code

---
+++ Source1.cpp
@@ -0,0 +1,33 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <string.h>
+#include <stack>
+
+
+int main()
+{	
+	std::stack<char *>  stek;
+	char ch[] = "asdas dasfsd fsdfs fergwr fwefw fwfwg4 56fef";
+	int i = strlen(ch);
+	char * point = strtok(ch ," ");
+	while (point != NULL)
+	{
+		stek.push(point);
+		point = strtok(NULL, " ");
+	}
+
+	std::string s;
+
+	while (!stek.empty())
+	{
+		s+=stek.top();
+		stek.pop();
+		if (!stek.empty())
+			s += ' ';
+	}
+
+	std::cout << s.c_str() << std::endl;
+	return 0;
+
+}
